4b823e9859a543d34508b2751fbaecda484846bd
Axel Steingrimsson <axel.steingrimsson@shopify.com>
2018-05-30 13:03:00 -0400

    Revert "increase read timeout from 1 -> 3 mins"

--- lib/azkaban_scheduler/client.rb
+++ lib/azkaban_scheduler/client.rb
@@ -10,7 +10,6 @@ module AzkabanScheduler
       @client_headers = client_headers
 
       @http = Net::HTTP.new(uri.host, uri.port)
-      @http.read_timeout = 180
       @http.use_ssl = uri.scheme == 'https'
 
       http_options.each do |key, value|
